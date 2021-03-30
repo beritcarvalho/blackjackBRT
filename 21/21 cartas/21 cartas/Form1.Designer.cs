d637c56d18cf6919a65eb0fbe3e9e94a0fbb375b
beritcarvalho <56369664+beritcarvalho@users.noreply.github.com>
2021-03-30 12:57:35 -0300

    Codigo
    Neste commit, escrevi o codigo principal

--- 21/21 cartas/21 cartas/Form1.Designer.cs
+++ 21/21 cartas/21 cartas/Form1.Designer.cs
@@ -30,75 +30,107 @@ namespace _21_cartas
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnJogar = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pibFundoCarta = new System.Windows.Forms.PictureBox();
+            this.pibCartas = new System.Windows.Forms.PictureBox();
+            this.lblPontos = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibFundoCarta)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibCartas)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // btnJogar
             // 
-            this.button1.Location = new System.Drawing.Point(105, 350);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 32);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btnJogar.Location = new System.Drawing.Point(10, 206);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(102, 32);
+            this.btnJogar.TabIndex = 0;
+            this.btnJogar.Text = "Jogar";
+            this.btnJogar.UseVisualStyleBackColor = true;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
             // 
-            // button2
+            // btnReiniciar
             // 
-            this.button2.Location = new System.Drawing.Point(212, 350);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(102, 32);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Enabled = false;
+            this.btnReiniciar.Location = new System.Drawing.Point(118, 206);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(102, 32);
+            this.btnReiniciar.TabIndex = 1;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(-4, -10);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(389, 464);
             this.pictureBox2.TabIndex = 2;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
-            // pictureBox1
+            // pibFundoCarta
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
-            this.pictureBox1.Image = global::_21_cartas.Properties.Resources.carta_vazia_com_sombra;
-            this.pictureBox1.Location = new System.Drawing.Point(139, 113);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 190);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.pibFundoCarta.BackColor = System.Drawing.SystemColors.Control;
+            this.pibFundoCarta.Image = global::_21_cartas.Properties.Resources.carta_vazia_com_sombra;
+            this.pibFundoCarta.Location = new System.Drawing.Point(45, 10);
+            this.pibFundoCarta.Name = "pibFundoCarta";
+            this.pibFundoCarta.Size = new System.Drawing.Size(150, 190);
+            this.pibFundoCarta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pibFundoCarta.TabIndex = 0;
+            this.pibFundoCarta.TabStop = false;
             // 
-            // pictureBox3
+            // pibCartas
             // 
-            this.pictureBox3.Image = global::_21_cartas.Properties.Resources._0;
-            this.pictureBox3.Location = new System.Drawing.Point(141, 115);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(139, 185);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.pibCartas.Image = global::_21_cartas.Properties.Resources._0;
+            this.pibCartas.Location = new System.Drawing.Point(47, 12);
+            this.pibCartas.Name = "pibCartas";
+            this.pibCartas.Size = new System.Drawing.Size(139, 185);
+            this.pibCartas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pibCartas.TabIndex = 3;
+            this.pibCartas.TabStop = false;
+            // 
+            // lblPontos
+            // 
+            this.lblPontos.AutoSize = true;
+            this.lblPontos.BackColor = System.Drawing.Color.Transparent;
+            this.lblPontos.Font = new System.Drawing.Font("Microsoft JhengHei", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblPontos.ForeColor = System.Drawing.Color.White;
+            this.lblPontos.Image = global::_21_cartas.Properties.Resources.fundo_verde;
+            this.lblPontos.Location = new System.Drawing.Point(90, 250);
+            this.lblPontos.Name = "lblPontos";
+            this.lblPontos.Size = new System.Drawing.Size(31, 35);
+            this.lblPontos.TabIndex = 4;
+            this.lblPontos.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Image = global::_21_cartas.Properties.Resources.fundo_verde;
+            this.label1.Location = new System.Drawing.Point(10, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 27);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Pontos: ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(413, 488);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(236, 299);
+            this.Controls.Add(this.lblPontos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pibCartas);
+            this.Controls.Add(this.pibFundoCarta);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnJogar);
             this.Controls.Add(this.pictureBox2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -107,19 +139,22 @@ namespace _21_cartas
             this.Text = "Black Jack (Versão 1.0)";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibFundoCarta)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pibCartas)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.Button btnReiniciar;
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pibFundoCarta;
+        private System.Windows.Forms.PictureBox pibCartas;
+        private System.Windows.Forms.Label lblPontos;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- 21/21 cartas/21 cartas/Form1.cs
+++ 21/21 cartas/21 cartas/Form1.cs
@@ -15,7 +15,11 @@ namespace _21_cartas
         public Form1()
         {
             InitializeComponent();
+            btnReiniciar.Enabled = false;
+            pibFundoCarta.Visible = false;            
         }
+        int pontos = 0;
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -27,8 +31,102 @@ namespace _21_cartas
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btnJogar_Click(object sender, EventArgs e)
         {
+            pibFundoCarta.Visible = true;
+            int numAleatorio;
+            Random sorteio = new Random();
+            numAleatorio = sorteio.Next(1, 14);
+            
+
+
+            switch (numAleatorio)
+            {
+                case 1:
+                    pibCartas.Image = Properties.Resources._01___ouro;
+                    pontos += 1;                    
+                    break;
+                case 2:
+                    pibCartas.Image = Properties.Resources._02___ouro;
+                    pontos += 2;
+                    break;
+
+                case 3:
+                    pibCartas.Image = Properties.Resources._03___ouro;
+                    pontos += 3;
+                    break;
+                case 4:
+                    pibCartas.Image = Properties.Resources._04___ouro;
+                    pontos += 4;
+                    break;
+                case 5:
+                    pibCartas.Image = Properties.Resources._05___ouro;
+                    pontos += 5;
+                    break;
+                case 6:
+                    pibCartas.Image = Properties.Resources._06___ouro;
+                    pontos += 6;
+                    break;
+                case 7:
+                    pibCartas.Image = Properties.Resources._07___ouro;
+                    pontos += 7;
+                    break;
+                case 8:
+                    pibCartas.Image = Properties.Resources._08___ouro;
+                    pontos += 8;
+                    break;
+                case 9:
+                    pibCartas.Image = Properties.Resources._09___ouro;
+                    pontos += 9;
+                    break;
+                case 10:
+                    pibCartas.Image = Properties.Resources._10___ouro;
+                    pontos += 10;
+                    break;
+                case 11:
+                    pibCartas.Image = Properties.Resources._11___ouro;
+                    pontos += 11;
+                    break;
+                case 12:
+                    pibCartas.Image = Properties.Resources._12___ouro;
+                    pontos += 12;
+                    break;
+                case 13:
+                    pibCartas.Image = Properties.Resources._13___ouro;
+                    pontos += 13;
+                    break;
+
+            }
+            lblPontos.Text = pontos.ToString();            
+
+            if (pontos <= 21)
+            {
+                if (pontos == 21) {
+                    lblPontos.Text = ("VOCÊ GANOU!!!");
+                    btnJogar.Enabled = false;
+                    btnReiniciar.Enabled = true;
+                }
+
+            }
+            else
+            {
+                lblPontos.Text = ("VOCÊ PERDEU!!!");
+                btnJogar.Enabled = false;
+                btnReiniciar.Enabled = true;
+                pibCartas.Visible = false;
+                pibFundoCarta.Visible = false;
+                lblPontos.Location = new Point(8,86);
+
+            }
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            pibFundoCarta.Visible = false;
+            pibCartas.Image = Properties.Resources._0;
+            pontos = 0;
+            lblPontos.Text = pontos.ToString();
+            btnJogar.Enabled = true;
 
         }
     }
